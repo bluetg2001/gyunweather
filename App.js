05c0fe872f9166059b4290ed9a29a927c868feff
bluetg2001 <45479579+bluetg2001@users.noreply.github.com>
2021-12-20 17:49:52 +0900

    first commit

--- App.js
+++ App.js
@@ -3,18 +3,10 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1}}>
+      <Text style={{flex:1}}>hhhhhhhhhhhi</Text>
+      <Text style={{flex:2 }}>hhhhhhhhhhhi</Text>
+      <Text style={{flex:1}}>hhhhhhhhhhhi</Text>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
